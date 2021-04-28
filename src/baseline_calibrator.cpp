dea6f28d18729463ef7d50fb188c697747e11abc
AndyHuang97 <huangandrea@hotmail.com>
2021-04-28 19:47:36 +0200

    Penalize linear trajectory in apparent baseline estimation

--- src/baseline_calibrator.cpp
+++ src/baseline_calibrator.cpp
@@ -25,11 +25,14 @@ static double WHEEL_RADIUS = 0.1575;
 static int count_gear = 0 ;
 static int count_base = 0 ;
 static double app_baseline = 0.0 ;
-static double gear_ratio = 0.0;
+static double gear_ratio = 0.02568;
 
 
 typedef message_filters::sync_policies
     ::ApproximateTime<MotorSpeed, MotorSpeed, MotorSpeed, MotorSpeed, nav_msgs::Odometry> MotorSyncPolicy;
+
+typedef message_filters::sync_policies
+    ::ApproximateTime<MotorSpeed, MotorSpeed, MotorSpeed, MotorSpeed, geometry_msgs::PoseStamped> MotorSyncPolicyGroundTruth;
 
 
 void onMotorMessagesSync(const MotorSpeedConstPtr& msg_fl, 
@@ -50,16 +53,16 @@ void onMotorMessagesSync(const MotorSpeedConstPtr& msg_fl,
 
     double rpm_l =  - (rpm_fl + rpm_bl) / 2;
     double rpm_r = (rpm_fr + rpm_br) / 2;
-    double new_gear_ratio = 0;
+    /*double new_gear_ratio = 0;
 
     if(abs(rpm_r+rpm_l) > 10e-6)
     {
-    new_gear_ratio = 2 * v_x / (WHEEL_RADIUS*(rpm_r + rpm_l));
+        new_gear_ratio = 2 * v_x / (WHEEL_RADIUS*(rpm_r + rpm_l));
 
-    gear_ratio = (gear_ratio * count_gear + abs(new_gear_ratio)) / (count_gear+1);
+        gear_ratio = (gear_ratio * count_gear + abs(new_gear_ratio)) / (count_gear+1);
 
-    count_gear ++;
-	}
+        count_gear ++;
+	}*/
 
 	double w_l = rpm_l * gear_ratio;
     double w_r = rpm_r * gear_ratio;
@@ -67,22 +70,121 @@ void onMotorMessagesSync(const MotorSpeedConstPtr& msg_fl,
     double v_left = w_l * WHEEL_RADIUS;
     double v_right = w_r * WHEEL_RADIUS;
 
-	
-	if(abs(w_z) > 10e-6)
+    //if (abs(v_right - v_left) < 10e-6 && abs(v_right - v_left) != 0.0) ROS_INFO("Wheel speed difference: %f",abs(v_right - v_left));
+
+	if(abs(w_z) > 10e-6 && abs(v_right - v_left) > 10e-2)
 	{
-    double new_app_baseline = (v_right - v_left) / w_z;
+        double new_app_baseline = (v_right - v_left) / w_z;
 
-    app_baseline = (app_baseline*count_base + abs(new_app_baseline))/(count_base+1);
+        if (new_app_baseline > 1.5) {
+            ROS_INFO("v_left: %f, v_right: %f, diff: %f", v_left, v_right, abs(v_right - v_left));
+            ROS_INFO("new_app: %f", new_app_baseline);
+        }
+        app_baseline = (app_baseline*count_base + abs(new_app_baseline))/(count_base+1);
 
-    count_base ++ ; 
+        count_base ++ ; 
     } 
     
 	
 	ROS_INFO("app: %f",app_baseline);
-	ROS_INFO("gear: %f",gear_ratio);
+	//ROS_INFO("gear: %f",gear_ratio);
 }
 
+static double lastStamp = 0.0;
+static double x = -0.832142;
+static double y = 0.426362;
+static double theta = 0.0;
+static bool initFlag = 1;
+static int occlusionThreshold = 2;
+static int occlusionCounter = 0;
+// TODO: how to set initial theta?
 
+void onMotorMessagesSyncGroundTruth(const MotorSpeedConstPtr& msg_fl, 
+                             const MotorSpeedConstPtr& msg_fr,
+                             const MotorSpeedConstPtr& msg_bl,
+                             const MotorSpeedConstPtr& msg_br,
+                             const geometry_msgs::PoseStamped::ConstPtr& msg) 
+{
+    //ROS_INFO("Motor: [%d:%d], gt_pose: [%d:%d]", msg_fl->header.stamp.sec, msg_fl->header.stamp.nsec, msg->header.stamp.sec, msg->header.stamp.nsec);
+    double rpm_fl, rpm_fr, rpm_bl, rpm_br;
+
+    rpm_fl = RPM_TO_RADS(msg_fl->rpm);// * GEAR_RATIO);
+    rpm_fr = RPM_TO_RADS(msg_fr->rpm);// * GEAR_RATIO);
+    rpm_bl = RPM_TO_RADS(msg_bl->rpm);// * GEAR_RATIO);
+    rpm_br = RPM_TO_RADS(msg_br->rpm);// * GEAR_RATIO);
+
+    double samplingTime;
+    samplingTime = lastStamp == 0 ? 0.0 : msg->header.stamp.toSec() - lastStamp;
+    lastStamp = msg->header.stamp.toSec();
+
+    double dx = msg->pose.position.x - x;
+    double dy = msg->pose.position.y - y;
+    double v_x = dx / samplingTime;
+    double v_y = dy / samplingTime;
+    double w_z = 0;
+    // TODO: maintain theta if robot does not move
+    double newTheta = atan2(dy, dx);
+    
+
+    if (occlusionCounter > occlusionThreshold) {
+        initFlag = 1;
+        //ROS_INFO("OCCLUSION DETECTED ...\n\n");
+    }
+    
+    ROS_INFO("Init theta: %f, initFlag: %d, occlusionCounter: %d", newTheta, initFlag, occlusionCounter);
+    if (initFlag && newTheta != 0.0) {
+        theta = newTheta;
+        w_z = 0;
+        initFlag = 0;
+        occlusionCounter = 0;
+        ROS_INFO("Init theta");
+    } else if (v_y != 0.0 && v_x != 0.0){
+        w_z = (newTheta - theta) / samplingTime;
+
+        x = msg->pose.position.x;
+        y = msg->pose.position.y;
+        theta = v_y != 0.0 && v_x != 0.0 ? newTheta : theta;
+        ROS_INFO("x: %f, y: %f, theta: %f", x, y, theta);
+
+        double rpm_l =  - (rpm_fl + rpm_bl) / 2;
+        double rpm_r = (rpm_fr + rpm_br) / 2;
+        double new_gear_ratio = 0;
+
+        if(abs(rpm_r+rpm_l) > 10e-6)
+        {
+            new_gear_ratio = 2 * v_x / (WHEEL_RADIUS*(rpm_r + rpm_l));
+
+            gear_ratio = (gear_ratio * count_gear + abs(new_gear_ratio)) / (count_gear+1);
+
+            count_gear ++;
+        }
+
+        double w_l = rpm_l * gear_ratio;
+        double w_r = rpm_r * gear_ratio;
+
+        double v_left = w_l * WHEEL_RADIUS;
+        double v_right = w_r * WHEEL_RADIUS;
+
+        
+        if(abs(w_z) > 10e-6)
+        {
+            double new_app_baseline = (v_right - v_left) / w_z;
+
+            app_baseline = (app_baseline*count_base + abs(new_app_baseline))/(count_base+1);
+
+            count_base ++ ; 
+        } 
+        
+        
+        ROS_INFO("app: %f",app_baseline);
+        ROS_INFO("gear: %f",gear_ratio);
+    
+    } else {
+        occlusionCounter++;
+    }
+    
+    
+}
 
 int main(int argc, char **argv)
 {
@@ -92,6 +194,7 @@ int main(int argc, char **argv)
 	ros::NodeHandle n;
   	//ros::Subscriber sub_base = n.subscribe("/scout_odom", 1000, addLine);
 	message_filters::Subscriber<nav_msgs::Odometry> sub_base(n, "/scout_odom", 1);
+    message_filters::Subscriber<geometry_msgs::PoseStamped> sub_gt_pose(n, "/gt_pose", 1);
 	message_filters::Subscriber<MotorSpeed> sub_fl(n, "/motor_speed_fl", 5); // front left topic
     message_filters::Subscriber<MotorSpeed> sub_fr(n, "/motor_speed_fr", 1); // front right topic
     message_filters::Subscriber<MotorSpeed> sub_bl(n, "/motor_speed_rl", 1); // front right topic
@@ -101,6 +204,8 @@ int main(int argc, char **argv)
     message_filters::Synchronizer<MotorSyncPolicy> sync(MotorSyncPolicy(25), sub_fl, sub_fr, sub_bl, sub_br, sub_base);
     sync.registerCallback(boost::bind(&onMotorMessagesSync, _1, _2, _3, _4, _5));
 
+    //message_filters::Synchronizer<MotorSyncPolicyGroundTruth> sync(MotorSyncPolicyGroundTruth(25), sub_fl, sub_fr, sub_bl, sub_br, sub_gt_pose);
+    //sync.registerCallback(boost::bind(&onMotorMessagesSyncGroundTruth, _1, _2, _3, _4, _5));
 	
 
 	ros::spin();

611582dfde26a630651d2d6d2f623684456ed1e7
MoioMan <cristianforzainter@gmail.com>
2021-05-01 16:05:37 +0200

    Update instruction and calibrator

--- src/baseline_calibrator.cpp
+++ src/baseline_calibrator.cpp
@@ -14,18 +14,16 @@
 
 
 #define RPM_TO_RADS(x)      x * (2 * M_PI) / 60 // 1 (rpm) : 2pi / 60 (rad/s) = n (rpm) : x (rad/s)
-
+#define ERR_THRESHOLD       1e-8
+#define EQUAL_WHEEL_SPEED_THRESHOLD 10e-2       // threshold under which the motion can be considered linear
 
 using namespace geometry_msgs;
 using namespace robotics_hw1;
 
 
-static double realBaseline = 0.583;
-static double WHEEL_RADIUS = 0.1575;
-static int count_gear = 0 ;
-static int count_base = 0 ;
-static double app_baseline = 0.0 ;
-static double gear_ratio = 0.02568;
+static double WHEEL_RADIUS;
+static double GEAR_RATIO;
+static bool use_fixed_gear_ratio = false;
 
 
 typedef message_filters::sync_policies
@@ -41,53 +39,53 @@ void onMotorMessagesSync(const MotorSpeedConstPtr& msg_fl,
                              const MotorSpeedConstPtr& msg_br,
                              const nav_msgs::Odometry::ConstPtr& msg) 
 {
+    static double app_baseline = 0.0;
+    static double gear_ratio;
+    static int count_gear = 0;
+    static int count_base = 0;
+
     double rpm_fl, rpm_fr, rpm_bl, rpm_br;
 
-    rpm_fl = RPM_TO_RADS(msg_fl->rpm);// * GEAR_RATIO);
-    rpm_fr = RPM_TO_RADS(msg_fr->rpm);// * GEAR_RATIO);
-    rpm_bl = RPM_TO_RADS(msg_bl->rpm);// * GEAR_RATIO);
-    rpm_br = RPM_TO_RADS(msg_br->rpm);// * GEAR_RATIO);
+    rpm_fl = RPM_TO_RADS(msg_fl->rpm);
+    rpm_fr = RPM_TO_RADS(msg_fr->rpm);
+    rpm_bl = RPM_TO_RADS(msg_bl->rpm);
+    rpm_br = RPM_TO_RADS(msg_br->rpm);
     
     double w_z = msg->twist.twist.angular.z;
     double v_x = msg->twist.twist.linear.x;
 
     double rpm_l =  - (rpm_fl + rpm_bl) / 2;
     double rpm_r = (rpm_fr + rpm_br) / 2;
-    /*double new_gear_ratio = 0;
+    double new_gear_ratio = 0;
 
-    if(abs(rpm_r+rpm_l) > 10e-6)
+    if(abs(rpm_r+rpm_l) > ERR_THRESHOLD)
     {
         new_gear_ratio = 2 * v_x / (WHEEL_RADIUS*(rpm_r + rpm_l));
-
-        gear_ratio = (gear_ratio * count_gear + abs(new_gear_ratio)) / (count_gear+1);
+        gear_ratio = (gear_ratio * count_gear + abs(new_gear_ratio)) / (count_gear+1);      // Incremental mean of gear ratio
 
         count_gear ++;
-	}*/
+	}
 
-	double w_l = rpm_l * gear_ratio;
-    double w_r = rpm_r * gear_ratio;
+    // If specified, use a fixed gear ratio (from rosparam); otherwise use the one which is currently computed
+    double gear_ratio_to_use = use_fixed_gear_ratio ? GEAR_RATIO : gear_ratio;  
+	double w_l = rpm_l * gear_ratio_to_use;
+    double w_r = rpm_r * gear_ratio_to_use;
 
     double v_left = w_l * WHEEL_RADIUS;
     double v_right = w_r * WHEEL_RADIUS;
 
-    //if (abs(v_right - v_left) < 10e-6 && abs(v_right - v_left) != 0.0) ROS_INFO("Wheel speed difference: %f",abs(v_right - v_left));
 
-	if(abs(w_z) > 10e-6 && abs(v_right - v_left) > 10e-2)
+	if(abs(w_z) > ERR_THRESHOLD && abs(v_right - v_left) > EQUAL_WHEEL_SPEED_THRESHOLD)
 	{
-        double new_app_baseline = (v_right - v_left) / w_z;
+        double new_app_baseline = (v_right - v_left) / w_z;            
+        app_baseline = (app_baseline*count_base + abs(new_app_baseline))/(count_base+1);    // Incremental mean of apparent baseline
 
-        if (new_app_baseline > 1.5) {
-            ROS_INFO("v_left: %f, v_right: %f, diff: %f", v_left, v_right, abs(v_right - v_left));
-            ROS_INFO("new_app: %f", new_app_baseline);
-        }
-        app_baseline = (app_baseline*count_base + abs(new_app_baseline))/(count_base+1);
-
-        count_base ++ ; 
+        count_base++; 
     } 
     
 	
-	ROS_INFO("app: %f",app_baseline);
-	//ROS_INFO("gear: %f",gear_ratio);
+	ROS_INFO("app: %f", app_baseline);
+	ROS_INFO("gear: %f", gear_ratio_to_use);
 }
 
 static double lastStamp = 0.0;
@@ -105,6 +103,11 @@ void onMotorMessagesSyncGroundTruth(const MotorSpeedConstPtr& msg_fl,
                              const MotorSpeedConstPtr& msg_br,
                              const geometry_msgs::PoseStamped::ConstPtr& msg) 
 {
+    static double app_baseline = 0.0;
+    static double gear_ratio;
+    static int count_gear = 0;
+    static int count_base = 0;
+
     //ROS_INFO("Motor: [%d:%d], gt_pose: [%d:%d]", msg_fl->header.stamp.sec, msg_fl->header.stamp.nsec, msg->header.stamp.sec, msg->header.stamp.nsec);
     double rpm_fl, rpm_fr, rpm_bl, rpm_br;
 
@@ -138,7 +141,7 @@ void onMotorMessagesSyncGroundTruth(const MotorSpeedConstPtr& msg_fl,
         initFlag = 0;
         occlusionCounter = 0;
         ROS_INFO("Init theta");
-    } else if (v_y != 0.0 && v_x != 0.0){
+    } else if (abs(v_y) > 1e-8 && abs(v_x) > 1e-8){
         w_z = (newTheta - theta) / samplingTime;
 
         x = msg->pose.position.x;
@@ -187,12 +190,32 @@ void onMotorMessagesSyncGroundTruth(const MotorSpeedConstPtr& msg_fl,
 }
 
 int main(int argc, char **argv)
-{
-  	
+{  	
 	ros::init(argc, argv, "listener");
 
 	ros::NodeHandle n;
-  	//ros::Subscriber sub_base = n.subscribe("/scout_odom", 1000, addLine);
+
+    // Read arg param
+    use_fixed_gear_ratio = false;
+    if (argc == 2)
+        use_fixed_gear_ratio = atoi(argv[1]) == 1;
+
+    ROS_INFO(use_fixed_gear_ratio ? "Using fixed gear ratio" : "Using online gear ratio");
+
+    // Read rosparams
+    bool ret = n.getParam("/gearRatio", GEAR_RATIO);    
+    if (!ret)
+    {
+        ROS_INFO("No gear ratio found");
+        use_fixed_gear_ratio = false;
+    }
+    ret = n.getParam("/wheelRadius", WHEEL_RADIUS);
+    if (!ret)
+    {
+        ROS_ERROR("Unable to get wheelRadius parameter!");
+        return -1;        
+    }
+
 	message_filters::Subscriber<nav_msgs::Odometry> sub_base(n, "/scout_odom", 1);
     message_filters::Subscriber<geometry_msgs::PoseStamped> sub_gt_pose(n, "/gt_pose", 1);
 	message_filters::Subscriber<MotorSpeed> sub_fl(n, "/motor_speed_fl", 5); // front left topic
@@ -209,7 +232,6 @@ int main(int argc, char **argv)
 	
 
 	ros::spin();
-
-  return 0;
+    return 0;
 }
 
